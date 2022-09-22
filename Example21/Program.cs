4f7d5c2ee9246cd7fe7f1d8e60bf41b296af3d38
ValeriyMakushev <makushevva@mail.ru>
2022-09-22 22:56:37 +0400

    Example21

---
+++ Example21/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("введите координату точки Xa: ");
+int Xa = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите координату точки Ya: ");
+int Ya = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите координату точки Xb: ");
+int Xb = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите координату точки Yb: ");
+int Yb = int.Parse(Console.ReadLine());
+
+double Ligth = Math.Sqrt (Math.Pow((Xb-Xa),2)+ Math.Pow((Yb-Ya),2));
+
+Console.WriteLine($"Ответ: {Ligth}" );
+
+
+
