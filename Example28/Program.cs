c44912cd2735de345caac5234c6e41e1fa048ec3
ValeriyMakushev <makushevva@mail.ru>
2022-09-26 22:39:42 +0400

    Example28

---
+++ Example28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и M и выдаёт произведение чисел от M до N.
+//2,4 -> 24 
+//1,5 -> 120
+
+Console.Write("Введите чило М: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите чило N: ");
+int N = int.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine(Multiplay(M,N));
+Console.WriteLine(Multiplay(1,5));
+int Multiplay(int from, int too)
+{
+    int sum = 1;
+    while (from <= too)
+
+    {
+        sum = sum * from;
+        from++;
+
+
+    }
+    return sum;
+}
